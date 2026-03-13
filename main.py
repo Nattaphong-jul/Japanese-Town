55c3ef7079ad1ffb4b7e8a7aab48144cb0b6d75d
Nattaphong-jul <nattapong.jullaya@gmail.com>
2026-03-13 11:23:35 +0700

    Added grab and move function

--- main.py
+++ main.py
@@ -193,7 +193,46 @@ def add_loop_cut(obj, edge_indices, cuts=1, offset=0.0):
     # Update the mesh and return to OBJECT mode
     bmesh.update_edit_mesh(obj.data)
     bpy.ops.object.mode_set(mode='OBJECT')
+
+def grab_move(obj, mode, index, direction, distance):
+    # Ensure we are in OBJECT mode
+    bpy.ops.object.mode_set(mode='OBJECT')
     
+    # Clear all selections
+    for v in obj.data.vertices: v.select = False
+    for e in obj.data.edges: e.select = False
+    for f in obj.data.polygons: f.select = False
+    
+    # Select the specified element based on mode and index
+    if mode == 'VERT':
+        obj.data.vertices[index].select = True
+    elif mode == 'EDGE':
+        obj.data.edges[index].select = True
+    elif mode == 'FACE':
+        obj.data.polygons[index].select = True
+    else:
+        print("Invalid mode. Use 'VERT', 'EDGE', or 'FACE'.")
+        return
+    
+    # Switch to EDIT mode
+    bpy.ops.object.mode_set(mode='EDIT')
+    
+    # Set the pivot point to individual origins for better control I guess
+    bpy.context.scene.tool_settings.transform_pivot_point = 'INDIVIDUAL_ORIGINS'
+    
+    # Move the selected element in the specified direction
+    if direction == 'UP':
+        bpy.ops.transform.translate(value=(0, 0, distance))
+    elif direction == 'DOWN':
+        bpy.ops.transform.translate(value=(0, 0, -distance))
+    elif direction == 'LEFT':
+        bpy.ops.transform.translate(value=(-distance, 0, 0))
+    elif direction == 'RIGHT':
+        bpy.ops.transform.translate(value=(distance, 0, 0))
+    
+    # Switch back to OBJECT mode
+    bpy.ops.object.mode_set(mode='OBJECT')
+
 
 index_overlay(True)
 
@@ -208,4 +247,8 @@ add_loop_cut(base, edge_indices=[0, 12, 4, 13], cuts=1, offset=0.2)
 bevel_vertices_ops(base, [0, 4, 3, 7], offset=0.2, segments=24)
 
 house_body = create_cube("House Body", (-1,1,1), (1.5,1.5,1))
-tri = create_triangle("Roof", (-1,1,2.5), (1.5,1.5,1))
+# tri = create_triangle("Roof", (-1,1,2.5), (1.5,1.5,1))
+
+add_loop_cut(house_body , edge_indices=[11, 5], cuts=1, offset=0)
+
+grab_move(house_body, 'EDGE', 14, 'UP', 1)
